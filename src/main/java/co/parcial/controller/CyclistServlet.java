b43c19356a5e480d6de21869cec7d41c9b902fc2
angieandreabayonavillabona <angelybayona15@gmail.com>
2021-05-20 09:14:23 -0400

    quinto commit

--- src/main/java/co/parcial/controller/CyclistServlet.java
+++ src/main/java/co/parcial/controller/CyclistServlet.java
@@ -12,10 +12,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import co.empresa.TestCrud.modelo.Usuario;
 import co.parcial.dao.countryDao;
 import co.parcial.dao.cyclistDao;
 import co.parcial.dao.teamDao;
 import co.parcial.modelo.Country;
+import co.parcial.modelo.Cyclist;
 import co.parcial.modelo.Team;
 
 /**
@@ -24,23 +26,31 @@ import co.parcial.modelo.Team;
 @WebServlet("/CyclistServlet")
 public class CyclistServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    private cyclistDao cyclistdao;
-    public CyclistServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    
-    public void init(ServletConfig config) throws ServletException {
+	private cyclistDao cyclistdao;
+	private teamDao teamdao;
+	private countryDao countrydao;
+	
+
+	public CyclistServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init(ServletConfig config) throws ServletException {
 		this.cyclistdao = new cyclistDao();
-	} 
+		this.countrydao = new countryDao();
+		this.teamdao = new teamDao();
+	}
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+
 		String action = request.getServletPath();
 		try {
 			switch (action) {
@@ -68,57 +78,68 @@ public class CyclistServlet extends HttpServlet {
 		}
 	}
 
-
-
-	
-
-
-	private void listCyclists(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
+	private void listCyclists(HttpServletRequest request, HttpServletResponse response) 
+			throws SQLException,ServletException, IOException{
+		List<Cyclist>listcyclist = cyclistdao.selectAll();
+		request.setAttribute("listcyclist", listcyclist);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("cyclistlist.jsp");
+		dispatcher.forward(request, response);
 		
+
 	}
 
-
-	private void actualizarCyclists(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		
+	private void actualizarCyclists(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		cyclistdao.delete(id);
+		response.sendRedirect("list");
 	}
 
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		Cyclist cyclistactual = cyclistdao.select(id);
+		request.setAttribute("cyclist", cyclistactual);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("usuario.jsp");
+		dispatcher.forward(request, response);
 
-	private void showEditForm(HttpServletRequest request, HttpServletResponse response) {
-		// TODO Auto-generated method stub
-		
 	}
 
-
-	private void eliminarCyclist(HttpServletRequest request, HttpServletResponse response) {
-	
-		
+	private void eliminarCyclist(HttpServletRequest request, HttpServletResponse response) throws SQLException, IOException {
+		int id = Integer.parseInt(request.getParameter("id"));
+		cyclistdao.delete(id);
+		response.sendRedirect("list");
 	}
 
-
-	private void insertarCyclist(HttpServletRequest request, HttpServletResponse response) {
-		
-		
+	private void insertarCyclist(HttpServletRequest request, HttpServletResponse response) 
+		throws SQLException, IOException {
+			String name = request.getParameter("nombre");
+			String email = request.getParameter("email");
+			String country = request.getParameter("country");
+			String birthdate = request.getParameter("birthdate");
+			String team = request.getParameter("team");
+			Cyclist cyclist = new Cyclist(0, name, email, birthdate, country, team);
+			cyclistdao.insert(cyclist);
+			response.sendRedirect("list");
 	}
 
-
-	private void showNewForm(HttpServletRequest request, HttpServletResponse response) 
-	throws SQLException,ServletException, IOException{
-		List<Country>listaCountrys = countryDao.selectAll();
-		List<Team> listaTeams = teamDao.selectAll();
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, ServletException, IOException {
+		List<Country> listaCountrys = countrydao.selectAll();
+		List<Team> listaTeams = teamdao.selectAll();
 		request.setAttribute("listaTeams", listaTeams);
 		request.setAttribute("listaCountrys", listaCountrys);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("cyclist.jsp");
 		dispatcher.forward(request, response);
-		
+
 	}
 
-
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
