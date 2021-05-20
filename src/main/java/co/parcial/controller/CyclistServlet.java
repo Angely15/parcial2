56d678f27c580194792f376122d8e7c34b10c9ab
angieandreabayonavillabona <angelybayona15@gmail.com>
2021-05-20 09:19:37 -0400

    sexto commit

--- src/main/java/co/parcial/controller/CyclistServlet.java
+++ src/main/java/co/parcial/controller/CyclistServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import co.empresa.TestCrud.modelo.Usuario;
 import co.parcial.dao.countryDao;
 import co.parcial.dao.cyclistDao;
 import co.parcial.dao.teamDao;
